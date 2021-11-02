c16e8192936d2c6fdd7e819c89208803f752772a
MagonxESP <janma.360@gmail.com>
2021-11-02 18:33:56 +0100

    añade el comando update en el script dotfiles.sh

--- share/scripts/dotfiles.sh
+++ share/scripts/dotfiles.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 case $1 in
+    "update")
+        git -C ${DOTFILES_ROOT_DIRECTORY} reset --hard HEAD
+        git -C ${DOTFILES_ROOT_DIRECTORY} pull origin master
+    ;;
     "reload-symlinks")
         python3 $DOTFILES_ROOT_DIRECTORY/share/scripts/symlinks.py
     ;;
@@ -9,7 +13,7 @@ case $1 in
     ;;
     "install-scripts-depencencies")
         pip3 install -r $DOTFILES_ROOT_DIRECTORY/share/scripts/requirements.txt
-    ;;    
+    ;;
     "-h")
         echo "Dotfiles"
         echo ""
@@ -21,11 +25,13 @@ case $1 in
         echo "  comando El nombre del comando a ejecutar para gestionar los dotfiles"
         echo ""
         echo "COMANDOS:"
-        echo "  reload-symlinks Actualiza los enlaces simbolicos de los dotfiles"
+        echo "  update  Actualiza todos los dotfiles a la ultima version, descartando cualquier cambio que no haya sido comiteado con antelación"
         echo ""
-        echo "  kitty-colors [fichero] Transforma un fichero exportado de terminal.sexy en formato JSON a el formato de la configuración de kitty"
+        echo "  reload-symlinks  Actualiza los enlaces simbolicos de los dotfiles"
         echo ""
-        echo "  install-scripts-depencencies Instala las dependecias de los scripts de python"
+        echo "  kitty-colors [fichero]  Transforma un fichero exportado de terminal.sexy en formato JSON a el formato de la configuración de kitty"
+        echo ""
+        echo "  install-scripts-depencencies  Instala las dependecias de los scripts de python"
     ;;    
     *)
         echo "El comando $1 no existe, usa la opcion -h para la ayuda"
