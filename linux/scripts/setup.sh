036d89366b1cd9bbb54ddbc7011eb8638f2e91a8
MagonxESP <janma.360@gmail.com>
2021-11-27 19:19:05 +0100

    remove sudo

--- linux/scripts/setup.sh
+++ linux/scripts/setup.sh
@@ -1,63 +1,68 @@
 #!/bin/bash
 
-# setup zsh
-sudo apt update
-
-echo "Instalando zsh..."
-sudo apt install -y zsh curl
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+apt update
 
 # setup packages
 echo "Instalando paquetes..."
-sudo apt install -y \
+apt install -y \
+  curl \
   flatpak \
+  gnome-software \
   gnome-software-plugin-flatpak \
+  gnome-tweaks \
   python3-pip \
   imagemagick \
   kitty \
   rofi \
   fonts-powerline \
   neofetch \
-  htop
+  htop \
+  nodejs \
+  npm
 
 
 # setup docker
 echo "Instalando docker..."
-sudo apt update
-sudo apt install \
+apt update
+apt install -y \
   ca-certificates \
   curl \
   gnupg \
   lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 
 echo "Instalando temas..."
-sudo apt install arc-theme
+apt install -y arc-theme
 
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt-get update
-sudo apt-get install papirus-icon-theme
+add-apt-repository ppa:papirus/papirus -y
+apt-get update
+apt-get install -y papirus-icon-theme
 
 echo "Instalando paquetes extra..."
 # lutris
-sudo add-apt-repository ppa:lutris-team/lutris
-sudo apt update
-sudo apt install lutris
+add-apt-repository ppa:lutris-team/lutris -y
+apt update
+apt install -y lutris
 
 # neovim
-sudo apt-add-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install neovim
+apt-add-repository ppa:neovim-ppa/stable -y
+apt update
+apt install -y neovim
+
+# setup zsh
+echo "Instalando zsh..."
+apt install -y zsh curl
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
