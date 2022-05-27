a362671666354e91dc2d230617b6a16089ad4632
MagonxESP <janma.360@gmail.com>
2022-05-27 21:52:33 +0200

    add sudo

--- linux/scripts/setup-ubuntu.sh
+++ linux/scripts/setup-ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt update
+sudo apt update
 
 # setup packages
 echo "Instalando paquetes..."
-apt install -y \
+sudo apt install -y \
   curl \
   python3 \
   python3-pip \
@@ -20,41 +20,41 @@ apt install -y \
 
 # setup docker
 echo "Instalando docker..."
-apt update
-apt install -y \
+sudo apt update
+sudo apt install -y \
   ca-certificates \
   curl \
   gnupg \
   lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 echo "Instalando temas..."
-apt install -y arc-theme
+sudo apt install -y arc-theme
 
-add-apt-repository ppa:papirus/papirus -y
-apt-get update
-apt-get install -y papirus-icon-theme
+sudo add-apt-repository ppa:papirus/papirus -y
+sudo apt-get update
+sudo apt-get install -y papirus-icon-theme
 
 echo "Instalando paquetes extra..."
 # neovim
-apt-add-repository ppa:neovim-ppa/stable -y
-apt update
-apt install -y neovim
+sudo apt-add-repository ppa:neovim-ppa/stable -y
+sudo apt update
+sudo apt install -y neovim
 
 # setup zsh
 echo "Instalando zsh..."
-apt install -y zsh curl
+sudo apt install -y zsh curl
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
