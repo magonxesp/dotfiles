19375addd761202d18c08a8eaaff4e628ebd70a5
MagonxESP <janma.360@gmail.com>
2022-07-14 19:04:26 +0200

    fix picom rounded corners

--- linux/shell/exports.sh
+++ linux/shell/exports.sh
@@ -4,9 +4,11 @@ if [[ -d "/usr/local/go/bin" ]]; then
     export PATH=$PATH:/usr/local/go/bin
 fi
 
-
 if [[ -f "$DOTFILES_DIRECTORY/shell/themes.sh" ]]; then
     source "$DOTFILES_DIRECTORY/shell/themes.sh"
 fi
 
 export NEOFETCH_IMAGE_BACKEND="kitty"
+
+# Awesome themmes path, uncomment if awesomewm is installed
+# export AWESOME_THEMES_PATH=$HOME/.config/awesome/themes
--- share/shell/shell.sh
+++ share/shell/shell.sh
@@ -3,3 +3,8 @@
 #
 # Shell startup script (like .profile)
 #
+source ${DOTFILES_ROOT_DIRECTORY}/share/shell/exports.sh
+
+if [[ -f $DOTFILES_DIRECTORY/shell/exports.sh ]]; then
+    source ${DOTFILES_DIRECTORY}/shell/exports.sh
+fi
