8f055bd8c47efa127149e1f1eef6839d55c2ba01
MagonxESP <janma.360@gmail.com>
2023-08-16 01:46:05 +0200

    add sdkman

--- share/shell/exports.sh
+++ share/shell/exports.sh
@@ -5,3 +5,8 @@
 if [[ -d "$HOME/.local/bin" ]]; then
     export PATH=$PATH:$HOME/.local/bin
 fi
+
+if [[ -d "$HOME/.sdkman" ]]; then
+    export SDKMAN_DIR="$HOME/.sdkman"
+    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
