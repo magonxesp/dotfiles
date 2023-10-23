5d8b8d89ca677a8cbed59f0b32104a4fd6e6a9bf
MagonxESP <janma.360@gmail.com>
2023-10-23 08:04:17 +0200

    remove bun export

--- share/shell/exports.sh
+++ share/shell/exports.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# export DEFAULT_USER="$(whoami)"
-
 if [[ -d "$HOME/.local/bin" ]]; then
     export PATH=$PATH:$HOME/.local/bin
-fi
-
-# Bun JavaScript runtime
-[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
-
-if [[ -d "$HOME/.bun" ]]; then
-    export BUN_INSTALL="$HOME/.bun"
-    export PATH="$BUN_INSTALL/bin:$PATH"
 fi
 
 if [[ -d "$HOME/.deno" ]]; then
