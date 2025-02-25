9568e4653a7e2aee48b25b0f0ea2be5c937c5490
juanmaalvarez <juanma.alvarez@basetis.com>
2025-02-25 10:01:32 +0100

    feat: delete repos alias

--- share/shell/alias.sh
+++ share/shell/alias.sh
@@ -6,11 +6,6 @@ if [[ $TERM == "xterm-kitty" ]]; then
     alias ssh="TERM=xterm-256color ssh"
 fi
 
-# alias for change to directories
-if [[ -d "/media/magonxesp/DATA/repos" ]]; then
-  alias repos="cd /media/magonxesp/DATA/repos"
-fi
-
 # alias for permissions
 alias own="sudo chown $USER:$USER"
 alias ownr="sudo chown -R $USER:$USER"
